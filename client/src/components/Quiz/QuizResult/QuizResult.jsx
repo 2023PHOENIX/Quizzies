b08656ab48df468798c816bd228ce60898056d37
abhishek <abhishekshrivastav1920@gmail.com>
2024-01-20 22:38:46 +0530

    feat: major feature in front-end done

---
+++ client/src/components/Quiz/QuizResult/QuizResult.jsx
@@ -0,0 +1,29 @@
+import trophy from "../../../assets/trophy.png";
+import styles from "./quiz.result.module.css";
+const QuizResult = ({ quizType }) => {
+  return (
+    <div className={styles.wrapper}>
+      {quizType === "q&a" && (
+        <>
+          <div className={styles.headingCongrats}>
+            Congrats Quiz is completed
+          </div>
+          <img src={trophy} className={styles.trophy} />
+          <div className={styles.score}>
+            your score is <span> 03/04</span>
+          </div>
+        </>
+      )}
+
+      {quizType === "poll" && (
+        <>
+          <div className={styles.pollHeading}>
+            Thank you for participating in the Poll
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default QuizResult;
--- client/src/pages/Quiz/Quiz.jsx
+++ client/src/pages/Quiz/Quiz.jsx
@@ -1,10 +1,17 @@
-import QuizInterface from "../../components/Quiz/QuizInterface";
+import { useState } from "react";
+import QuizInterface from "../../components/Quiz/QuizInterface/QuizInterface";
 import styles from "./quiz.module.css";
-
+import QuizResult from "../../components/Quiz/QuizResult/QuizResult";
 const Quiz = () => {
+  const [finalPage, setFinalPage] = useState(false);
+  const [quizType, setQuizType] = useState("poll");
   return (
     <div className={styles.wrapper}>
-      <QuizInterface />
+      <div className={styles.innerWrapper}>
+        {!finalPage && <QuizInterface />}
+
+        {finalPage && <QuizResult quizType={quizType} />}
+      </div>
     </div>
   );
 };
